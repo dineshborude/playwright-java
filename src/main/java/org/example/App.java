3692ba70d7c69c2ed2b19bc57a1305437b2b9b26
Dinesh Borude <79857147+dineshborude@users.noreply.github.com>
2024-02-28 20:06:49 +0530

    First Test

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import com.microsoft.playwright.*;
+
+public class App {
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch();
+            Page page = browser.newPage();
+            page.navigate("http://playwright.dev");
+            System.out.println(page.title());
+        }
+    }
+}
