b49a0beab4469d74f74714406ff2b531a6b46040
Dinesh Borude <79857147+dineshborude@users.noreply.github.com>
2024-02-29 19:37:13 +0530

    Starting with Swag Labs

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,9 +5,12 @@ import com.microsoft.playwright.*;
 public class App {
     public static void main(String[] args) {
         try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.chromium().launch();
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+
             Page page = browser.newPage();
-            page.navigate("http://playwright.dev");
+
+            page.navigate("https://www.saucedemo.com/v1/");
+
             System.out.println(page.title());
         }
     }
