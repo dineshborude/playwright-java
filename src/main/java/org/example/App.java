c0749b384bdacb1c2b484590d7c9980d81705175
Dinesh Borude <79857147+dineshborude@users.noreply.github.com>
2024-02-29 19:59:58 +0530

    Added WebKit Browser

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,7 +7,10 @@ import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertTha
 public class App {
     public static void main(String[] args) {
         try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+//            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+
+            Browser browser = playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(false));
+
 
             Page page = browser.newPage();
 
